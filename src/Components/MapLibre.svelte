25c17a5016522a799424c8ec12918cfa2fb7ae81
BenOnTrack <81491865+BenOnTrack@users.noreply.github.com>
2023-11-26 22:04:09 +1300

    add components

--- src/Components/MapLibre.svelte
+++ src/Components/MapLibre.svelte
@@ -1,7 +1,18 @@
+
 <script context="module" lang="ts">
     import maplibre from 'maplibre-gl';
     import { page } from '$app/stores';
     import { PMTiles } from '$lib/pmtiles/pmtiles';
+
+	import LayerColorSwitcher from './LayerColorSwitcher.svelte';
+	import LayerSwitcher from './LayerSwitcher.svelte';
+	import RouteDropdown from './RouteDropdown.svelte';
+	import { Tag } from 'carbon-components-svelte';
+
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import 'carbon-components-svelte/css/white.css';
+
+	import {getDataSetFeatureFromID,toggleRouteAndSetMapViewFromFilter} from '../utils/MapFunctions.svelte'
 
     const basemapFilePath = 'basemap.pmtiles';
     const routeFilePath = 'route.pmtiles';
@@ -51,7 +62,7 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 
-	import { Map } from 'maplibre-gl';
+	import { Map, NavigationControl } from 'maplibre-gl';
 	import { browser } from '$app/environment';
 	import { base } from '$app/paths';
 	import { DBFetchSource } from '$lib/pmtiles/dbsource';
@@ -65,10 +76,42 @@
 	}
 	// const { Map /*GeolocateControl */ } = maplibre;
 
-	let container: HTMLDivElement;
+	// Map
+	let mapContainer: HTMLDivElement;
 	let map: Map;
 	let pmtilesOrigin: string;
 	let staticOrigin: string;
+	let targetLayers: string[];
+	let clickedSourceFeature: maplibregl.GeoJSONFeature;
+	let clickedDataSetFeature = null;
+	let bookmarks = {};
+
+	interface StyleLayer {
+		id: number;
+		text: string;
+		visibility: string;
+	}
+	let styleLayers: StyleLayer[];
+	let layerColorSwitcherIds: StyleLayer[] = [];
+	let layerSwitcherIds: StyleLayer[] = [];
+	let layerSwitcherSelectedIds: number[];
+	interface Layer {
+		id: string;
+		metadata?: { switch: boolean };
+	}
+	// Tags
+	let tagList: string[] = [];
+
+	// RouteSwitcher
+	let routeDataSetFeatures = [];
+	let filteredRouteDataSetFeatures = [];
+	let groupedFilteredRouteDataSetFeatures = {};
+	let routeMin = 0;
+	let routeMax = 1;
+	const nice = (d) => {
+		if (!d && d !== 0) return '';
+		return d.toFixed(2);
+	};
 
 	onMount(async () => {
 		console.log('Page Url Origin:', $page.url.origin);
@@ -88,8 +131,49 @@
 		style.sprite = style.sprite.replace('@staticOrigin@', staticOrigin);
 		style.glyphs = style.glyphs.replace('@staticOrigin@', staticOrigin);
 
+		// Style Layers
+		styleLayers = style.layers.map((layer: { id: number }, id: number) => ({
+			id,
+			text: layer.id,
+			visibility: layer.layout?.visibility || null
+		}));
+		// Layer Color Switcher
+		layerColorSwitcherIds = styleLayers.filter((layer) => {
+			const metadata = style.layers[layer.id]?.metadata;
+			return metadata && metadata.color === true;
+		});
+
+		// Layer Switcher
+		layerSwitcherIds = styleLayers.filter((layer) => {
+			const layerData = style.layers[layer.id];
+			return layerData.metadata && layerData.metadata.switch === true;
+		});
+		layerSwitcherSelectedIds = layerSwitcherIds
+			.filter((layer) => layer.visibility === 'visible')
+			.map((layer) => layer.id);
+
+		// Route Switcher
+		routeDataSetFeatures = await (await fetch(`${base}/route_dataset.json`)).json();
+		console.log('routeDataSet:', routeDataSetFeatures);
+		// filteredRouteDataSetFeatures contains the filtered set
+		filteredRouteDataSetFeatures = routeDataSetFeatures;
+		// Group the features by category
+		filteredRouteDataSetFeatures.forEach((feature) => {
+			if (!groupedFilteredRouteDataSetFeatures[feature.subclass]) {
+				groupedFilteredRouteDataSetFeatures[feature.subclass] = [];
+			}
+			groupedFilteredRouteDataSetFeatures[feature.subclass].push(feature);
+		});
+
+		// Bookmarks
+		bookmarks = await (await fetch(`${base}/bookmarks.geojson`)).json();
+		console.log('bookmarks:', bookmarks);
+		let bounds = new maplibre.LngLatBounds([174.398279, -37.104532], [175.33349, -36.828027]);
+		targetLayers = ['poi-food_and_drink', 'poi-lodging', 'poi-transportation'];
+
+		// Map
 		map = new Map({
-			container: container,
+			container: mapContainer,
 			style: style,
 			center: [lon, lat],
 			// zoom: zoom,
@@ -97,14 +181,98 @@
 			refreshExpiredTiles: false,
 			maxBounds: bounds
 		});
+
+		map.on('load', function () {
+			// control
+			map.addControl(new NavigationControl(), 'top-right');
+			map.addControl(
+				new maplibre.GeolocateControl({
+					positionOptions: {
+						enableHighAccuracy: true
+					},
+					trackUserLocation: true
+				})
+			);
+
+			// POI
+			map.on('click', (e) => {
+				let features = map.queryRenderedFeatures(e.point, {
+					layers: targetLayers
+				});
+				if (!features.length) return;
+				else {
+					console.log(features[0]);
+					clickedSourceFeature = features[0];
+					map.flyTo({
+						center: clickedSourceFeature.geometry.coordinates,
+						zoom: 20
+					});
+					updateFeatureInfo();
+				}
+			});
+
+			// Route_midpoint
+			map.on('click', (e) => {
+				let renderedSourceFeatures = map.queryRenderedFeatures(e.point, {
+					layers: ['route_midpoint']
+				});
+				if (!renderedSourceFeatures.length) return;
+				else {
+					clickedSourceFeature = renderedSourceFeatures[0];
+					console.log('clickedSourceFeature', clickedSourceFeature);
+					clickedDataSetFeature = getDataSetFeatureFromID(
+						filteredRouteDataSetFeatures,
+						clickedSourceFeature.properties.id
+					);
+					const { id, type, bbox, members } = clickedDataSetFeature;
+					console.log('clickedDataSetFeature', clickedDataSetFeature);
+					const routeData = {
+						id: id,
+						type: type,
+						bbox: bbox,
+						zoom: 14
+					};
+					const filterWayMembers = ['in', ['id'], ['literal', members]];
+					const filterRelation = ['==', 'id', id];
+					toggleRouteAndSetMapViewFromFilter(map, filterWayMembers, filterRelation, bbox);
+				}
+			});
+		});
 	});
+
+	function updateFeatureInfo() {
+		tagList = [];
+		const propertiesToInclude = ['name:latin', 'class', 'subclass', 'category', 'cuisine'];
+
+		propertiesToInclude.forEach((property) => {
+			const value = clickedSourceFeature['properties'][property];
+			if (value !== undefined && value !== null && value !== '') {
+				tagList.push(value);
+			}
+		});
+	}
+		
+
 </script>
 
 <svelte:head>
-	<link rel="stylesheet" href="https://cdn.skypack.dev/maplibre-gl/dist/maplibre-gl.css" />
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </svelte:head>
 
-<div class="mapContainer" bind:this={container} />
+<div>
+	<div id="map" bind:this={mapContainer} />
+	<LayerColorSwitcher {map} {layerColorSwitcherIds} />
+	<LayerSwitcher {map} {layerSwitcherIds} {layerSwitcherSelectedIds} />
+	<div id="feature-info">
+		<RouteDropdown {map} {filteredRouteDataSetFeatures} {groupedFilteredRouteDataSetFeatures} />
+
+		{#if tagList.length > 0}
+			{#each tagList as tag (tag)}
+				<Tag>{tag}</Tag>
+			{/each}
+		{/if}
+	</div>
+</div>
 
 {#await basemapPMSource?.fileInfoPromise}
 	<div class="waiting">
@@ -113,12 +281,6 @@
 {/await}
 
 <style>
-	.mapContainer {
-		position: absolute;
-		top: 0;
-		bottom: 0;
-		width: 100%;
-	}
 	.waiting {
 		/* position: absolute;
 		top: 50%;
@@ -131,6 +293,13 @@
 		top: 50%;
 		left: 50%;
 		transform: translate(-50%, -50%);
+		padding: 10px;
+	}
+	#map {
+		width: 100%; /* Adjust as needed */
+		height: 80vh; /* Adjust as needed */
+	}
+	#feature-info {
 		padding: 10px;
 	}
 </style>
