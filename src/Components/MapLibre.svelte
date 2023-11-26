13594474a0c5d831310351e8aeee49452b716975
BenOnTrack <81491865+BenOnTrack@users.noreply.github.com>
2023-11-26 17:25:22 +1300

    first commit

--- src/Components/MapLibre.svelte
+++ src/Components/MapLibre.svelte
@@ -1,34 +1,52 @@
 <script context="module" lang="ts">
-	import maplibre from 'maplibre-gl';
-	import { page } from '$app/stores';
+    import maplibre from 'maplibre-gl';
+    import { page } from '$app/stores';
+    import { PMTiles } from '$lib/pmtiles/pmtiles';
 
-	import { PMTiles } from '$lib/pmtiles/pmtiles';
+    const basemapFilePath = 'basemap.pmtiles';
+    const routeFilePath = 'route.pmtiles';
 
-	const filePath = 'source.pmtiles';
-	const pmSource = browser ? new DBFetchSource(filePath) : null;
+    const basemapPMSource = browser ? new DBFetchSource(basemapFilePath) : null;
+    const routePMSource = browser ? new DBFetchSource(routeFilePath) : null;
 
-	const pmTiles = new PMTiles(browser ? pmSource : filePath);
+    const basemapPMTiles = new PMTiles(browser ? basemapPMSource : basemapFilePath);
+    const routePMTiles = new PMTiles(browser ? routePMSource : routeFilePath);
 
-	if (browser) {
-		maplibre.addProtocol('pmtiles', (params, callback) => {
-			const filePath = params.url.split('://')[1];
-			const arg = params.url.match(/\/([0-9]+)\/([0-9]+)\/([0-9]+)\.pbf/);
-			if (arg?.length != 4) return callback(new Error(`Tile fetch error: bad params`));
-			const z = parseInt(arg[1]);
-			const x = parseInt(arg[2]);
-			const y = parseInt(arg[3]);
-			const pmres = pmTiles.getZxy(z, x, y).then((t) => {
-				if (t) {
-					callback(null, t.data, null, null);
-				} else {
-					console.log(`Didn't get ${z}, ${x}, ${y}`);
-					callback(null, null, null, null);
-				}
-			});
-			return { cancel: () => {} };
-		});
-	}
+    function handleTileRequest(match, pmTiles: PMTiles): Promise<any> {
+        const z = parseInt(match[1]);
+        const x = parseInt(match[2]);
+        const y = parseInt(match[3]);
+
+        return pmTiles.getZxy(z, x, y);
+    }
+
+    if (browser) {
+        maplibre.addProtocol('pmtiles', (params, callback) => {
+            const matchBasemap = params.url.match(/\/basemap\/([0-9]+)\/([0-9]+)\/([0-9]+)\.pbf/);
+            const matchRoute = params.url.match(/\/route\/([0-9]+)\/([0-9]+)\/([0-9]+)\.pbf/);
+
+            Promise.all([
+                matchBasemap && handleTileRequest(matchBasemap, basemapPMTiles),
+                matchRoute && handleTileRequest(matchRoute, routePMTiles)
+            ]).then(([tileBasemap, tileRoute]) => {
+                if (tileBasemap) {
+                    callback(null, tileBasemap.data, null, null);
+                } else if (tileRoute) {
+                    callback(null, tileRoute.data, null, null);
+                } else {
+                    console.log('No tiles fetched for either basemap or route');
+                    callback(null, null, null, null);
+                }
+            }).catch(error => {
+                console.error(`Error fetching tiles: ${error.message}`);
+                callback(error, null, null, null);
+            });
+
+            return { cancel: () => {} };
+        });
+    }
 </script>
+
 
 <script lang="ts">
 	import { onMount } from 'svelte';
@@ -38,10 +56,10 @@
 	import { base } from '$app/paths';
 	import { DBFetchSource } from '$lib/pmtiles/dbsource';
 
-	export let lat = 40;
-	export let lon = -105.18;
-	export let zoom = 5;
-
+	export let lat = -36.88;
+	export let lon = 174.77;
+	export let zoom = 12;
+	let bounds = new maplibre.LngLatBounds([174.398279, -37.104532], [175.33349, -36.828027]);
 	$: if (map) {
 		map.flyTo({ center: [lon, lat], zoom: zoom, essential: true });
 	}
@@ -49,17 +67,26 @@
 
 	let container: HTMLDivElement;
 	let map: Map;
+	let pmtilesOrigin: string;
+	let staticOrigin: string;
 
 	onMount(async () => {
-		console.log('Base path', base);
-		await pmSource?.fileInfoPromise;
+		console.log('Page Url Origin:', $page.url.origin);
+		console.log('Base path:', base);
+		pmtilesOrigin = `pmtiles://${$page.url.origin.split('://')[1]}`;
+		staticOrigin = `${$page.url.origin}${base}`;
+		await basemapPMSource?.fileInfoPromise;
+		await routePMSource?.fileInfoPromise;
+
 		const style = await (await fetch(`${base}/mystyle.json`)).json();
-		// Replace the origin in the template file with the page origin, as it is where the tile endpoint
-		// run.
-		style.sources.openmaptiles.tiles = style.sources.openmaptiles.tiles.map((s: string) => {
-			const customOrigin = `pmtiles://${$page.url.origin.split('://')[1]}`;
-			return s.replace('@origin@', customOrigin);
+		style.sources.basemap.tiles = style.sources.basemap.tiles.map((s: string) => {
+			return s.replace('@pmtilesOrigin@', pmtilesOrigin);
 		});
+		style.sources.route.tiles = style.sources.route.tiles.map((s: string) => {
+			return s.replace('@pmtilesOrigin@', pmtilesOrigin);
+		});
+		style.sprite = style.sprite.replace('@staticOrigin@', staticOrigin);
+		style.glyphs = style.glyphs.replace('@staticOrigin@', staticOrigin);
 
 		map = new Map({
 			container: container,
@@ -67,7 +94,8 @@
 			center: [lon, lat],
 			// zoom: zoom,
 			// maxTileCacheSize: 5000,
-			refreshExpiredTiles: false
+			refreshExpiredTiles: false,
+			maxBounds: bounds
 		});
 	});
 </script>
@@ -78,7 +106,7 @@
 
 <div class="mapContainer" bind:this={container} />
 
-{#await pmSource?.fileInfoPromise}
+{#await basemapPMSource?.fileInfoPromise}
 	<div class="waiting">
 		<h1>Please Wait</h1>
 	</div>
