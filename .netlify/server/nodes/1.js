eae26e53d879a67ee90ca5b6d6e64b6d26783d49
BenOnTrack <81491865+BenOnTrack@users.noreply.github.com>
2023-11-28 22:51:15 +1300

    add gitignore

--- .netlify/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.bfeafbcc.js","_app/immutable/chunks/index.cb889fce.js","_app/immutable/chunks/stores.8f770604.js","_app/immutable/chunks/singletons.8ec05b82.js"];
-export const stylesheets = [];
-export const fonts = [];
